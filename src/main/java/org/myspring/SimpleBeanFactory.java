df7dbbf4d4840a51c9828437d55877eab1674154
zed <1319850155@qq.com>
2023-04-04 01:22:42 +0800

    在bean factory的基础上实现bean的单例获取，即getBean时使用的name相同则获取的对象相同。

--- src/main/java/org/myspring/SimpleBeanFactory.java
+++ src/main/java/org/myspring/SimpleBeanFactory.java
@@ -7,15 +7,26 @@ import java.util.Map;
 
 public class SimpleBeanFactory {
     private Map<String, BeanDefinition> beanDefinitionHashMap = new HashMap<>();
+    private Map<String, Object> beanHashMap = new HashMap<>();
 
     public void registBean(String beanName, BeanDefinition beanDefinition) {
         beanDefinitionHashMap.put(beanName, beanDefinition);
     }
 
-    public Object getBean(String beanName) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public Object getBean(String beanName) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Object bean = beanHashMap.get(beanName);
+        if(bean == null) {
+            bean = createBean(beanName);
+        }
+        return bean;
+    }
+
+    private Object createBean(String beanName) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         BeanDefinition beanDefinition = beanDefinitionHashMap.get(beanName);
         Class<?> beanClass = beanDefinition.getBeanClass();
         Constructor<?> constructor = beanClass.getDeclaredConstructor();
-        return constructor.newInstance();
+        Object bean = constructor.newInstance();
+        beanHashMap.put(beanName, bean);
+        return bean;
     }
 }
---
+++ src/test/java/org/myspring/test/ApiTest/SingletonBeanTest.java
@@ -0,0 +1,23 @@
+package org.myspring.test.ApiTest;
+
+import org.myspring.BeanDefinition;
+import org.myspring.SimpleBeanFactory;
+import org.myspring.test.bean.UserService;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class SingletonBeanTest {
+    @Test
+    public void test_SingletonBean() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        SimpleBeanFactory simpleBeanFactory = new SimpleBeanFactory();
+        BeanDefinition beanDefinition = new BeanDefinition(Class.forName("org.myspring.test.bean.UserService"));
+        simpleBeanFactory.registBean("userService", beanDefinition);
+        UserService userService1 = (UserService) simpleBeanFactory.getBean("userService");
+        userService1.queryUserInfo();
+        UserService userService2 = (UserService) simpleBeanFactory.getBean("userService");
+        assertEquals(userService1, userService2);
+
+    }
+}
